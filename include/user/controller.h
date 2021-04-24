b041c6ea0df08c3394173eb952d03e38273a1a0d
Joshua Shew <joshua.t.shew@gmail.com>
2021-04-24 13:29:56 +0900

    moved code into new files

---
+++ include/user/controller.h
@@ -0,0 +1,13 @@
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+
+// controler variables for funsies
+extern int axis1;
+extern int axis2;
+extern int axis3;
+extern int axis4;
+
+// to update controller related variables
+void controller_variables_update();
+
+#endif
--- src/user/controller.cpp
+++ src/user/controller.cpp
@@ -1,11 +1,17 @@
-// controler variables for funsies
-int axis1;
-int axis2;
-int axis3;
-int axis4;
+#include "vex.h"
+#include "robot-config.h"
 
-    // controller variables update
-    axis1 = ctlr1.Axis1.value();
-    axis2 = ctlr1.Axis2.value();
-    axis3 = ctlr1.Axis3.value();
-    axis4 = ctlr1.Axis4.value();
+// controler variables for funsies
+int axis1 = 0;
+int axis2 = 0;
+int axis3 = 0;
+int axis4 = 0;
+
+// to update controller related variables
+void controller_variables_update()
+{
+  axis1 = ctlr1.Axis1.value();
+  axis2 = ctlr1.Axis2.value();
+  axis3 = ctlr1.Axis3.value();
+  axis4 = ctlr1.Axis4.value();
+}
