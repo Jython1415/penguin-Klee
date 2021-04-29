1f9f58815dee01af252d805ca6ab4483d3ce60cc
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2021-04-30 08:53:02 +0900

    Remove controller-mapping code

--- src/user/usercontrol.cpp
+++ src/user/usercontrol.cpp
@@ -2,27 +2,10 @@
 #include "robot-config.h"
 #include "motors.h"
 #include "user/controller.h"
+#include "user/controller-mapping.h"
 
 // linear mapping constants
 int linear_b_threshold = 15; // linear bottom threshold
-
-// curve mapping constants
-bool curve_is_on = 1; // on and off for curved controller mapping
-double c_exponent = 2.0;
-int c_lower_dz = 10; // lower dead zone; dead zone being the part of the joystick range that has no effect
-int c_upper_dz = 10; // upper dead zone
-int c_lowest_pwr = 3; // lowest power output
-// derived using desmos.com/calculator/rzxfys8ctn -- a calculator I developed for this purpose
-double c_multiplier = 0;
-// math for the mapping
-int c_equation (int axis_input)
-{
-  if (abs(axis_input) >= 127 - c_upper_dz) { return copysign(100, axis_input); }
-  else if (abs(axis_input) <= c_lower_dz) { return 0; }
-  else { return int(copysign(round(c_multiplier * pow(abs(axis_input) - c_lower_dz, c_exponent)) + c_lowest_pwr, axis_input)); }
-}
-// initiaizing the mapping constant
-void c_mapping_initialize() { c_multiplier = (100 - c_lowest_pwr) / pow(127 - (c_upper_dz + c_lower_dz), c_exponent); }
 
 // speed multiplier
 double chassis_speed_multiplier = 1.0;
