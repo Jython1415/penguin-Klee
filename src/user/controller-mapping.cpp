011a755ed1cb45b457dc6aa3301451279068cccd
Joshua Shew <joshua.t.shew@gmail.com>
2021-05-04 18:53:53 +0900

    updated function and debugging

--- src/user/controller-mapping.cpp
+++ src/user/controller-mapping.cpp
@@ -10,6 +10,10 @@ int c_lowest_pwr = 3; // lowest power output
 // derived using desmos.com/calculator/rzxfys8ctn -- a calculator I developed for this purpose
 double c_multiplier = 0;
 
+// mapping array
+int c_map_array[255];
+int c_map (int input) { return c_map_array[input + 127]; }
+
 // math for the mapping
 int c_equation (int axis_input)
 {
@@ -19,20 +23,36 @@ int c_equation (int axis_input)
 }
 
 // initiaizing the mapping constant
-void c_mapping_initialize() { c_multiplier = (100 - c_lowest_pwr) / pow(127 - (c_upper_dz + c_lower_dz), c_exponent); }
-
-void c_mapping_print_map()
+void c_mapping_initialize()
 {
-  // array for mapping
-  int c_map[255];
+  c_multiplier = (100 - c_lowest_pwr) / pow(127 - (c_upper_dz + c_lower_dz), c_exponent);
 
   // add values to the array
   for (int i = 0; i < 255; i++) {
-    c_map[i] = c_equation(i - 127);
+    c_map_array[i] = c_equation(i - 127);
+    if (i % 10 == 0) { vex::task::sleep(1); } // to make sure the program doesn't cut off the loop for hogging too much time
+  }
+}
+
+void c_mapping_print_map()
+{
+  // print the array
+  std::cout << "Controller Mapping Array (CMA)" << std::endl;
+
+  for (int i = 0; i < 255; i++) {
+    std::cout << i - 127 << ", " << c_map_array[i] << std::endl; // prints: axis, power
+    if (i % 10 == 0) { vex::task::sleep(1); } // to make sure the program doesn't cut off the loop for hogging too much time
   }
 
-  // print the array
-  for (int i = 0; i < 255; i++) {
-    std::cout << i - 127 << ", " << c_map[i] << std::endl; // prints: axis, power
+  // points of interest (POI) print
+  std::cout << "Points of Interest on the CMA" << std::endl;
+
+  const int POI_len = 9;
+  int POI[POI_len] = {0, c_lower_dz - 1, c_lower_dz, c_lower_dz + 1, 63, (127 - c_upper_dz) - 1, (127 - c_upper_dz), (127 - c_upper_dz) + 1, 127};
+
+  for (int i = 0; i < POI_len; i++) {
+    std::cout << POI[i] << ", " << c_map(POI[i]);
+    if (POI[i] != 0) { std::cout << ", " << c_map(POI[i] * -1) << std::endl; }
+    else { std::cout << std::endl; }
   }
 }
